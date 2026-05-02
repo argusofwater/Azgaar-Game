fe70cd8f906094a264f69f1720458a8046796ac9
argusofwater <argusofwater@gmail.com>
2026-05-01 21:18:11 -0600

    Add basic state module

---
+++ src/state.js
@@ -0,0 +1,11 @@
+export let state = {
+  nations: {},
+  provinces: [],
+  relations: {},
+  playerNation: null,
+  turn: 1
+};
+
+export function initGame() {
+  console.log('Game initialized');
+}
